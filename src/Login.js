b073e555911845dd4dcc2695d604c737b1b96f1b
Lindsay White <lindsaywhite@Lindsays-Air.hsd1.fl.comcast.net>
2021-04-29 18:41:11 -0400

    login/signup

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,52 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
 export default function Home(){
+    const [login, setLogin]= useState({
+        username:'',
+        password:''
+    })
+
+    const handleChange=(e)=>{
+        setLogin({...login, [e.target.name]: e.target.value});
+        
+    }
+
+    const onSubmit=(e)=>{
+        e.preventDefault();
+            console.log("form submitted");
+            axios.post('https://reqres.in/api/users', login)
+                .then(response=>console.log(response))
+                .catch(err=>console.log(err));
+    }
+
     return(
-        <h1>
-            Login!!!
-        </h1>
+        <div className='contianer'>
+            <h1>
+                Login!
+            </h1>
+            <form onSubmit={onSubmit}>
+                
+            <label>
+                Username: 
+            <input 
+            type ='text'
+            name = 'username'
+            value={login.username}
+            onChange={handleChange}
+            />
+            </label>
+            <label>
+                Password: 
+            <input 
+            type ='password'
+            name = 'password'
+            value={login.password}
+            onChange={handleChange}
+            />
+            </label>
+            <button type='submit'>Submit</button>
+        </form>
+    </div>
     )
 }
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,20 +1,33 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function SignUp(){
     const [signup, setSignup]= useState({
         firstName:'',
         lastName:'',
         email:'',
-        password:'',
-        cfPassword:'',
-        phoneNum:''
+        password:''
     })
+
+    const handleChange=(e)=>{
+        setSignup({...signup, [e.target.name]: e.target.value});
+        
+    }
+
+    const onSubmit=(e)=>{
+        e.preventDefault();
+        console.log(signup.firstName, signup.lastName, signup.email, signup.password)
+            console.log("form submitted");
+            axios.post('https://reqres.in/api/users', signup)
+                .then(response=>console.log(response))
+                .catch(err=>console.log(err));
+    }
     return(
         <div class='container'>
         <h1>
             Sign up!
             </h1>
-        <form>
+        <form onSubmit={onSubmit}>
             
             <label>
                 First Name: 
@@ -22,9 +35,37 @@ export default function SignUp(){
             type ='text'
             name = 'firstName'
             value={signup.firstName}
-            // onChange={handleChange}
+            onChange={handleChange}
             />
             </label>
+            <label>
+                Last Name: 
+            <input 
+            type ='text'
+            name = 'lastName'
+            value={signup.lastName}
+            onChange={handleChange}
+            />
+            </label>
+            <label>
+                Email: 
+            <input 
+            type ='email'
+            name = 'email'
+            value={signup.email}
+            onChange={handleChange}
+            />
+            </label>
+            <label>
+                Password: 
+            <input 
+            type ='password'
+            name = 'password'
+            value={signup.password}
+            onChange={handleChange}
+            />
+            </label>
+            <button type='submit'>Submit</button>
         </form>
         </div>
     );
